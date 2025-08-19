7db37b39652a38e23a94978f94c9e4882ebd1973
Bartosz <holzturbs@gmail.com>
2025-08-19 18:34:44 +0200

    finished project

--- src/main.js
+++ src/main.js
@@ -7,22 +7,58 @@ const rangeRed = document.querySelector('.red');
 const rangeGreen = document.querySelector('.green');
 const rangeBlue = document.querySelector('.blue');
 
+let red = 128;
+let green = 128;
+let blue = 128;
+
+// console.log(red.toString(16));
+
+function randomColor() {
+	red = Math.round(Math.random() * 255);
+	blue = Math.round(Math.random() * 255);
+	green = Math.round(Math.random() * 255);
+
+	rangeRed.value = red;
+	rangeBlue.value = blue;
+	rangeGreen.value = green;
+
+	createBackground();
+}
+
+randomButton.addEventListener('click', randomColor);
 // 1. Z czego zbudowany jest hex?
 // 2. Czy range ma value?
 
-// body.style.background = 'red';
+// body.style.background = 'rgb(red, green, blue)';
+
+// rgb(red, green, blue)
+
+function createBackground() {
+	const background = `rgb(${red}, ${green}, ${blue})`;
+
+	body.style.background = background;
+
+	const text = `#${red.toString(16).padStart(2, 0)}${green.toString(16).padStart(2, 0)}${blue
+		.toString(16)
+		.padStart(2, 0)}`;
+
+	screen.textContent = text;
+}
 
 rangeRed.addEventListener('input', event => {
-	console.log(event.target.value);
+	red = parseInt(event.target.value);
+	createBackground();
 });
 
 rangeGreen.addEventListener('input', event => {
-	console.log(event.target.value);
+	green = Number(event.target.value);
+	createBackground();
 });
 
 rangeBlue.addEventListener('input', event => {
-	console.log(event.target.value);
+	blue = +event.target.value;
+	createBackground();
 });
 
-//1. Zrobić to samo tylko z pozostałymi suwakami. 
-//2. Wrzucić zmiany na githuba. 
+//1. Zrobić to samo tylko z pozostałymi suwakami.
+//2. Wrzucić zmiany na githuba.
